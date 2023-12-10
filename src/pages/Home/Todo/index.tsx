8b8b4c3ed1c50f4aac50a620b94f4c4fca989b7b
mAmidMokhtari <m.amid.mokhtari@gmail.com>
2023-12-11 01:57:33 +0330

    - implement todo features

---
+++ src/pages/Home/Todo/index.tsx
@@ -0,0 +1,110 @@
+import {
+  Add as AddIcon,
+  CheckBox,
+  Close as CloseIcon,
+  Create as CreateIcon,
+} from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  TextField,
+} from "@mui/material";
+
+import { useData } from "./useData";
+
+const Todo = () => {
+  const data = useData();
+
+  return (
+    <Box
+      width="100%"
+      maxWidth="lg"
+      bgcolor="background.paper"
+      boxShadow={10}
+      p={2}
+    >
+      <Box display="flex" alignItems="center" mt={2}>
+        <TextField
+          type="text"
+          value={data.inputText}
+          onChange={data.handleInputChange}
+          placeholder="Please insert note..."
+          variant="outlined"
+          fullWidth
+          sx={{ marginRight: "1rem" }}
+        />
+        <Button
+          variant="contained"
+          color="success"
+          size="large"
+          onClick={data.handleAddTodo}
+        >
+          <AddIcon />
+        </Button>
+      </Box>
+      <List sx={{ marginTop: "1rem" }}>
+        {data.todos.map((todo) => (
+          <ListItem
+            key={todo.id}
+            sx={{
+              my: 2,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              borderRadius: "0.5rem",
+              backgroundColor: "lightblue",
+              boxShadow: 4,
+              p: 2,
+            }}
+          >
+            {data.editingTodoId === todo.id ? (
+              <>
+                <TextField
+                  type="text"
+                  value={data.editingText}
+                  onChange={(event) => data.setEditingText(event.target.value)}
+                  variant="outlined"
+                  fullWidth
+                  sx={{ marginRight: "1rem" }}
+                />
+                <IconButton
+                  color="primary"
+                  size="large"
+                  onClick={data.handleUpdateTodo}
+                >
+                  <CheckBox />
+                </IconButton>
+              </>
+            ) : (
+              <>
+                <ListItemText sx={{ flex: "1 1 50%" }}>
+                  {todo.text}
+                </ListItemText>
+                <IconButton
+                  color="info"
+                  size="large"
+                  onClick={() => data.handleEditTodo(todo.id)}
+                >
+                  <CreateIcon />
+                </IconButton>
+                <IconButton
+                  color="error"
+                  size="large"
+                  onClick={() => data.handleDeleteTodo(todo.id)}
+                >
+                  <CloseIcon />
+                </IconButton>
+              </>
+            )}
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+};
+
+export default Todo;
---
+++ src/pages/Home/Todo/useData.ts
@@ -0,0 +1,82 @@
+import { useState } from "react";
+
+import { defer } from "react-router-dom";
+
+import { http } from "../../../providers/api/http";
+import { Todo } from "../../../services/utils/types";
+
+export const useData = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const [inputText, setInputText] = useState("");
+
+  const [editingTodoId, setEditingTodoId] = useState<number | null>(null);
+
+  const [editingText, setEditingText] = useState("");
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (inputText.trim() === "") {
+      return;
+    }
+
+    const newTodo: Todo = {
+      id: Date.now(),
+      text: inputText,
+    };
+
+    setTodos([...todos, newTodo]);
+    setInputText("");
+  };
+
+  const handleDeleteTodo = (id: number) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  const handleEditTodo = (id: number) => {
+    const todoToEdit = todos.find((todo) => todo.id === id);
+
+    if (todoToEdit) {
+      setEditingTodoId(id);
+      setEditingText(todoToEdit.text);
+    }
+  };
+
+  const handleUpdateTodo = () => {
+    if (editingTodoId !== null) {
+      setTodos((prevTodos) =>
+        prevTodos.map((todo) =>
+          todo.id === editingTodoId ? { ...todo, text: editingText } : todo
+        )
+      );
+      setEditingTodoId(null);
+      setEditingText("");
+    }
+  };
+  return {
+    todos,
+    inputText,
+    editingTodoId,
+    editingText,
+    setEditingText,
+    handleInputChange,
+    handleAddTodo,
+    handleDeleteTodo,
+    handleEditTodo,
+    handleUpdateTodo,
+  };
+};
+
+export async function todoLoader() {
+  return defer({
+    todo: loadTodo(),
+  });
+}
+
+const loadTodo = async () => {
+  const response = await http.get("/todos");
+  return response.data;
+};
