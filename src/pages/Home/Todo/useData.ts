1b314bf87c40a67ae47c118251c3212dac6317de
mAmidMokhtari <m.amid.mokhtari@gmail.com>
2023-12-12 01:08:01 +0330

    debugging

--- src/pages/Home/Todo/useData.ts
+++ src/pages/Home/Todo/useData.ts
@@ -21,7 +21,6 @@ export const useData = () => {
     mutationFn: postTodo,
     onSuccess: (data: Todo) => {
       data && setTodos((prevState) => [data, ...prevState]);
-
       setInputText("");
     },
   });
